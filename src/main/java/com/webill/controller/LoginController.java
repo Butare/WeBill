33e3e28a410962630ca275e39ed929446054be78
Butare <bjimsons@gmail.com>
2017-01-25 23:57:51 +0900

    controller to redirect user to show uploaded page

--- src/main/java/com/webill/controller/LoginController.java
+++ src/main/java/com/webill/controller/LoginController.java
@@ -32,86 +32,93 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 @SessionAttributes({"userName"})
-public class LoginController{
-    
+public class LoginController {
+
     @Autowired
     LoginFormValidation loginFormValidation;
-    
-    @Autowired 
-    private UserDao jdbcDao;
- 
- @RequestMapping(value="/login", method = RequestMethod.GET) 
- public String getLoginPage(Model model){
-     User user = new User();
-      
-     model.addAttribute("userLogin", user);
-        return "Login"; 
-   }
- 
- @RequestMapping(value="/login", method = RequestMethod.POST)
- public ModelAndView validateLoginPage(@Valid @ModelAttribute("userLogin") User user, 
-                                        BindingResult result, Model model) 
-                                        throws ClassNotFoundException, SQLException{
-     
-     Logger l = Logger.getLogger("Test");
-     l.log(Level.INFO, "User Role :{0}", user.getUserRole());
-    
-     // model data
-     Map modelM = model.asMap();
-     for (Object modelKey : modelM.keySet()) {
-         Object modelValue = modelM.get(modelKey);
-         System.out.println(modelKey +"  :Model key and values are: "+ modelValue);
-     }
-     
-     
-     ModelAndView mv = null;
-     
-     loginFormValidation.validate(user, result);
-     
-     if(result.hasErrors()){
-         mv = new ModelAndView("Login");
-         return mv;
-     }
-     
-     if (!user.getUserID().isEmpty() && !user.getPassWord().isEmpty() && !user.getUserRole().isEmpty()) 
-     {
 
-     List<Map<String, Object>> successUser = jdbcDao.getUserLogin(user.getUserID(), user.getPassWord(), user.getUserRole());
-        if (successUser != null && !successUser.isEmpty()) {
-            
-            Map<String, Object> userLogin = successUser.get(0);
-                
+    @Autowired
+    private UserDao jdbcDao;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String getLoginPage(Model model) {
+        User user = new User();
+
+        model.addAttribute("userLogin", user);
+        return "Login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView validateLoginPage(@Valid @ModelAttribute("userLogin") User user,
+            BindingResult result, Model model)
+            throws ClassNotFoundException, SQLException {
+
+        Logger l = Logger.getLogger("Test");
+        l.log(Level.INFO, "User Role :{0}", user.getUserRole());
+
+        // model data
+        Map modelM = model.asMap();
+        for (Object modelKey : modelM.keySet()) {
+            Object modelValue = modelM.get(modelKey);
+            System.out.println(modelKey + "  :Model key and values are: " + modelValue);
+        }
+
+        ModelAndView mv = null;
+
+        loginFormValidation.validate(user, result);
+
+        if (result.hasErrors()) {
+            mv = new ModelAndView("Login");
+            return mv;
+        }
+
+        if (!user.getUserID().isEmpty() && !user.getPassWord().isEmpty() && !user.getUserRole().isEmpty()) {
+
+            List<Map<String, Object>> successUser = jdbcDao.getUserLogin(user.getUserID(), user.getPassWord(), user.getUserRole());
+            if (successUser != null && !successUser.isEmpty()) {
+
+                Map<String, Object> userLogin = successUser.get(0);
+
                 // session 
                 model.addAttribute("userName", user.getUserID());
                 //model.addAttribute("userID", USER)
-                
+
                 // model attribute
                 model.addAttribute("role", userLogin.get("userRole"));
                 model.addAttribute("givenName", userLogin.get("givenName"));
                 model.addAttribute("meterID", userLogin.get("meterID"));
-                
+
                 if (user.getUserRole().equals(ADMIN_ROLE)) {
                     mv = new ModelAndView("/userList");
                     mv.addObject("userList", jdbcDao.getUserList());
 
-                    
                 } else if (user.getUserRole().equals(CUSTOMER_ROLE)) {
-                    mv = new ModelAndView("welcome");
+                    mv = new ModelAndView(showUploaded(user.getUserID(), model));
+                    
+                    //redirect error check.
                 }
-                
+
                 l.log(Level.INFO, "The user. {0} successfully loged in.", userLogin.get("givenName"));
 
-        }else{
+            } else {
                 model.addAttribute("errorMessage", "Incorrect username/password. Try again");
                 mv = new ModelAndView("Login");
             }
-     }else{
+        } else {
             model.addAttribute("errorMessage", "Please input all values. Try again");
             mv = new ModelAndView("Login");
         }
-     
-     return mv;
 
-    }      
+        return mv;
+
+    }
+
+    @RequestMapping(value = "/showUploaded", method = RequestMethod.POST)
+    public String showUploaded(String userID, Model model) {
+        
+         model.addAttribute("listImageDetails", jdbcDao.getImageDetailByUser(userID));
+
+        return "imageDetails";
+
+    }
 }
-    
