f5b3dd970c9c1651398b5004876cc55342e5be97
Butare <bjimsons@gmail.com>
2017-01-25 23:58:44 +0900

    controls added

--- src/main/java/com/webill/controller/QrReaderController.java
+++ src/main/java/com/webill/controller/QrReaderController.java
@@ -5,65 +5,40 @@
  */
 package com.webill.controller;
 
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
-import com.google.zxing.RGBLuminanceSource;
-import com.google.zxing.Result;
-import com.google.zxing.common.HybridBinarizer;
-import com.webill.utils.Constants;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
+import com.webill.utils.QrReader;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
- *
+ * 
  * @author JimmyHome
+ * 
+ * This request handler class prints the scanned meter ID from qr image.
  */
 @Controller
 @SessionAttributes("imageName")
 public class QrReaderController {
-    
-    @RequestMapping(value ="/readQrCode" , method = RequestMethod.POST)
+
+    @RequestMapping(value = "/readQrCode", method = RequestMethod.POST)
     @ResponseBody
-    public String getQrCode (HttpServletRequest request, @ModelAttribute("imageName") String imageName) throws NotFoundException{
-         File imageFile = new File(request.getSession().
-                    getServletContext().getRealPath(Constants.UPLOAD_DIRECTORY) + "/_" + imageName);
-         BufferedImage bufImage;
-         BinaryBitmap bitMap;
-         Result result;
-         
-         
-         String stringOutput = null;
-         
-        try {
-            bufImage = ImageIO.read(imageFile);
-            
-            if (bufImage == null) {
-                stringOutput = "No bitmap detected. Probably No QR code included";
-            } else {
-                int[] pixels = bufImage.getRGB(0, 0, bufImage.getWidth(), bufImage.getHeight(), null, 0, bufImage.getWidth());
-            RGBLuminanceSource source = new RGBLuminanceSource(bufImage.getWidth(), bufImage.getHeight(), pixels);
-                bitMap = new BinaryBitmap(new HybridBinarizer(source));
-                MultiFormatReader reader = new MultiFormatReader();
-                result = reader.decode(bitMap);
-                stringOutput = "The QR code reads "+ result.getText();
-            }
-            
-        } catch (IOException ex) {
-            Logger.getLogger(QrReaderController.class.getName()).log(Level.SEVERE, null, ex);
+    public String getQrCode(HttpServletRequest request, @ModelAttribute("imageName") String imageName, @RequestParam("userID")String userID) throws NotFoundException {
+
+        String stringOutput;
+
+        if (QrReader.getQrCode(request, imageName, userID) == null) {
+            stringOutput = "No bitmap detected. Probably No QR code included";
+        } else {
+            stringOutput = "The Code reads is : " + QrReader.getQrCode(request, imageName, userID);
         }
-    return stringOutput;
-}
-    
+
+        return stringOutput;
+
+    }
 }
