999f845cc6310074f2308b4f6fc5ce0dac7cf904
Butare <bjimsons@gmail.com>
2017-01-25 23:59:25 +0900

    new updates

--- src/main/java/com/webill/daoApi/UserDao.java
+++ src/main/java/com/webill/daoApi/UserDao.java
@@ -5,6 +5,7 @@
  */
 package com.webill.daoApi;
 
+import com.webill.model.ImageDetail;
 import com.webill.model.User;
 import java.util.List;
 import java.util.Map;
@@ -21,4 +22,7 @@ public interface UserDao {
     public void createUserTable( String userID);
     public boolean isGpsInformationExist(double longitude, double latitude);
     public void addGpsInformation(String userID, double longitude, double latitude);
+    public boolean isQRCodeExist(String qrCode);
+    public void insertImageDetails(String userID, String imageName);
+    public List<ImageDetail> getImageDetailByUser(String userID);
 }
